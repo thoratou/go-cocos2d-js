01c1296fea2376e2afb7f11c2f35093f09c2b8b2
thoratou <thoratou@gmail.com>
2015-07-11 16:53:53 +0200

    added almost all Menu items

--- cc/CCMenuItem.go
+++ cc/CCMenuItem.go
@@ -1,9 +1,5 @@
 package cc
 
-import (
-	"github.com/gopherjs/gopherjs/js"
-)
-
 //////////////
 // MenuItem //
 //////////////
@@ -15,17 +11,17 @@ type MenuItem interface {
 	//IsOpacityModifyRGB() bool defined as part of Node
 	IsEnabled() bool
 	SetEnabled(bool)
-	InitWithCallback(func(*js.Object), Node) bool
+	InitWithCallback(func(), Node) bool
 	Rect() Rect
 	Selected()
 	Unselected()
-	SetCallback(func(*js.Object), Node)
+	SetCallback(func(), Node)
 	Activate()
 }
 
 type menuItem struct{ node }
 
-func NewMenuItem(callback func(*js.Object), target Node) MenuItem {
+func NewMenuItem(callback func(...interface{}), target Node) MenuItem {
 	return &menuItem{node{pcc.Get("MenuItem").New(callback, target)}}
 }
 
@@ -49,8 +45,8 @@ func (mi *menuItem) SetEnabled(enable bool) {
 	mi.Call("setEnabled", enable)
 }
 
-func (mi *menuItem) InitWithCallback(callback func(*js.Object), target Node) bool {
-	return mi.Call("isEnabled", callback, target).Bool()
+func (mi *menuItem) InitWithCallback(callback func(), target Node) bool {
+	return mi.Call("initWithCallback", callback, target).Bool()
 }
 
 func (mi *menuItem) Rect() Rect {
@@ -65,7 +61,7 @@ func (mi *menuItem) Unselected() {
 	mi.Call("unselected")
 }
 
-func (mi *menuItem) SetCallback(callback func(*js.Object), target Node) {
+func (mi *menuItem) SetCallback(callback func(), target Node) {
 	mi.Call("setCallback", callback, target)
 }
 
@@ -77,19 +73,136 @@ func (mi *menuItem) Activate() {
 // MenuItemLabel //
 ///////////////////
 
-//TODO MenuItemLabel
+type MenuItemLabel interface {
+	MenuItem
+	GetDisabledColor() Color
+	SetDisabledColor(Color)
+	GetLabel() Node
+	SetLabel(Node)
+	//SetEnabled(bool) defined as part of Node
+	//GetOpacity() int defined as part of Node
+	//SetOpacity(int) defined as part of Node
+	//GetColor() Color defined as part of Node
+	//SetColor(Color) defined as part of Node
+	//initWithLabel
+	SetString(string)
+	GetString() string
+	//Activate() defined as part of Node
+	//Selected() defined as part of Node
+	//Unselected() defined as part of Node
+}
+
+type menuItemLabel struct{ menuItem }
+
+func NewMenuItemLabel(label Node, selector func(), target Node) MenuItemLabel {
+	return &menuItemLabel{menuItem{node{pcc.Get("MenuItemLabel").New(label, selector, target)}}}
+}
+
+func (m *menuItemLabel) GetDisabledColor() Color {
+	return &color{m.Call("getDisabledColor")}
+}
+
+func (m *menuItemLabel) SetDisabledColor(color Color) {
+	m.Call("setDisabledColor", color)
+}
+
+func (m *menuItemLabel) GetLabel() Node {
+	return &node{m.Call("getLabel")}
+}
+
+func (m *menuItemLabel) SetLabel(label Node) {
+	m.Call("setLabel", label)
+}
+
+func (m *menuItemLabel) SetEnabled(enabled bool) {
+	m.Call("setEnabled", enabled)
+}
+
+func (m *menuItemLabel) GetOpacity() int {
+	return m.Call("getOpacity").Int()
+}
+
+func (m *menuItemLabel) SetOpacity(opacity int) {
+	m.Call("setOpacity", opacity)
+}
+
+func (m *menuItemLabel) GetColor() Color {
+	return &color{m.Call("getColor")}
+}
+
+func (m *menuItemLabel) SetColor(color Color) {
+	m.Call("setColor", color)
+}
+
+func (m *menuItemLabel) SetString(label string) {
+	m.Call("SetString", label)
+}
+
+func (m *menuItemLabel) GetString() string {
+	return m.Call("getString").String()
+}
+
+func (m *menuItemLabel) Activate() {
+	m.Call("activate")
+}
+
+func (m *menuItemLabel) Selected() {
+	m.Call("selected")
+}
+
+func (m *menuItemLabel) Unselected() {
+	m.Call("unselected")
+}
 
 ///////////////////////
 // MenuItemAtlasFont //
 ///////////////////////
 
-//TODO MenuItemAtlasFont
+type MenuItemAtlasFont interface {
+	MenuItemLabel
+	//initWithString
+}
+
+type menuItemAtlasFont struct{ menuItemLabel }
+
+func NewMenuItemAtlasFont(value string, charMapFile string, itemWidth float64, itemHeight float64, startCharMap string, callback func(), target Node) MenuItemAtlasFont {
+	return &menuItemAtlasFont{menuItemLabel{menuItem{node{pcc.Get("MenuItemAtlasFont").New(value, charMapFile, itemWidth, itemHeight, startCharMap, callback, target)}}}}
+}
 
 //////////////////
 // MenuItemFont //
 //////////////////
 
-//TODO MenuItemFont
+type MenuItemFont interface {
+	MenuItemLabel
+	//initWithString
+	SetFontSize(int)
+	GetFontSize() int
+	SetFontName(string)
+	GetFontName() string
+}
+
+type menuItemFont struct{ menuItemLabel }
+
+func NewMenuItemFont(value string, callback func(), target Node) MenuItemFont {
+	return &menuItemFont{menuItemLabel{menuItem{node{pcc.Get("MenuItemFont").New(value, callback, target)}}}}
+}
+
+func (m *menuItemFont) SetFontSize(s int) {
+	m.Call("setFontSize", s)
+}
+
+func (m *menuItemFont) GetFontSize() int {
+	return m.Call("getFontSize").Int()
+}
+
+func (m *menuItemFont) SetFontName(name string) {
+	m.Call("setFontName", name)
+}
+
+func (m *menuItemFont) GetFontName() string {
+	return m.Call("getFontName").String()
+}
 
 ////////////////////
 // MenuItemSprite //
@@ -97,13 +210,84 @@ func (mi *menuItem) Activate() {
 
 type MenuItemSprite interface {
 	MenuItem
+	GetNormalImage() Sprite
+	SetNormalImage(Sprite)
+	GetSelectedImage() Sprite
+	SetSelectedImage(Sprite)
+	GetDisabledImage() Sprite
+	SetDisabledImage(Sprite)
+	//SetColor(Color) defined as part of Node
+	//GetColor() Color defined as part of Node
+	//SetOpacity(int) defined as part of Node
+	//GetOpacity() int defined as part of Node
+	//Selected() defined as part of Node
+	//Unselected() defined as part of Node
+	//SetEnabled(bool) defined as part of Node
 }
 
 type menuItemSprite struct{ menuItem }
 
-//func NewMenuItemSprite() MenuItemSprite {
-//	return &menuItemSprite{menuItem{node{pcc.Get("MenuItemSprite").New()}}}
-//}
+// NewMenuItemSprite is the constructor of cc.MenuItemSprite
+// {Image|Null} normalSprite normal state image
+// {Image|Null} selectedSprite selected state image
+// {Image|Null} disabled state image
+// {function|Null} callback
+// {cc.Node|Null} target Node
+func NewMenuItemSprite(normalImage *string, selectedImage *string, disabledImage *string, callback *func(), target Node) MenuItemSprite {
+	return &menuItemSprite{menuItem{node{pcc.Get("MenuItemSprite").New(normalImage, selectedImage, disabledImage, callback, target)}}}
+}
+
+func (m *menuItemSprite) GetNormalImage() Sprite {
+	return &sprite{node{m.Call("getNormalImage")}}
+}
+
+func (m *menuItemSprite) SetNormalImage(normalImage Sprite) {
+	m.Call("setNormalImage", normalImage)
+}
+
+func (m *menuItemSprite) GetSelectedImage() Sprite {
+	return &sprite{node{m.Call("getSelectedImage")}}
+}
+
+func (m *menuItemSprite) SetSelectedImage(selectedImage Sprite) {
+	m.Call("setSelectedImage", selectedImage)
+}
+
+func (m *menuItemSprite) GetDisabledImage() Sprite {
+	return &sprite{node{m.Call("getDisabledImage")}}
+}
+
+func (m *menuItemSprite) SetDisabledImage(disabledImage Sprite) {
+	m.Call("setDisabledImage", disabledImage)
+}
+
+func (m *menuItemSprite) SetColor(color Color) {
+	m.Call("setColor", color)
+}
+
+func (m *menuItemSprite) GetColor() Color {
+	return &color{m.Call("getColor")}
+}
+
+func (m *menuItemSprite) SetOpacity(opacity int) {
+	m.Call("setOpacity", opacity)
+}
+
+func (m *menuItemSprite) GetOpacity() int {
+	return m.Call("getOpacity").Int()
+}
+
+func (m *menuItemSprite) Selected() {
+	m.Call("selected")
+}
+
+func (m *menuItemSprite) Unselected() {
+	m.Call("unselected")
+}
+
+func (m *menuItemSprite) SetEnabled(enabled bool) {
+	m.Call("setEnabled", enabled)
+}
 
 ///////////////////
 // MenuItemImage //
@@ -111,16 +295,57 @@ type menuItemSprite struct{ menuItem }
 
 type MenuItemImage interface {
 	MenuItemSprite
+	SetNormalSpriteFrame(SpriteFrame)
+	SetSelectedSpriteFrame(SpriteFrame)
+	SetDisabledSpriteFrame(SpriteFrame)
 }
 
 type menuItemImage struct{ menuItemSprite }
 
+// NewMenuItemImage is the constructor of cc.MenuItemImage
+// {Image|Null} normalSprite normal state image
+// {Image|Null} selectedSprite selected state image
+// {Image|Null} disabled state image
+// {function|Null} callback
+// {cc.Node|Null} target Node
 func NewMenuItemImage(normalImage *string, selectedImage *string, disabledImage *string, callback *func(), target Node) MenuItemImage {
 	return &menuItemImage{menuItemSprite{menuItem{node{pcc.Get("MenuItemImage").New(normalImage, selectedImage, disabledImage, callback, target)}}}}
+}
+
+func (m *menuItemImage) SetNormalSpriteFrame(frame SpriteFrame) {
+	m.Call("setNormalSpriteFrame", frame)
+}
+
+func (m *menuItemImage) SetSelectedSpriteFrame(frame SpriteFrame) {
+	m.Call("setSelectedSpriteFrame", frame)
+}
+
+func (m *menuItemImage) SetDisabledSpriteFrame(frame SpriteFrame) {
+	m.Call("setDisabledSpriteFrame", frame)
 }
 
 ////////////////////
 // MenuItemToggle //
 ////////////////////
 
-//TODO MenuItemToggle
+type MenuItemToggle interface {
+	MenuItem
+	//GetOpacity() int defined as part of Node
+	//SetOpacity(int) defined as part of Node
+	//GetColor() Color defined as part of Node
+	//SetColor(Color) defined as part of Node
+	GetSelectedIndex() int
+	SetSelectedIndex(int)
+	GetSubItems() []MenuItem
+	SetSubItems([]MenuItem)
+	//initWithItems
+	AddSubItem(MenuItem)
+	//Activate() defined as part of Node
+	//Selected() defined as part of Node
+	//Unselected() defined as part of Node
+	//SetEnabled(bool) defined as part of Node
+	GetSelectedItem() MenuItem
+	//SetOnEnter(func()) defined as part of Node
+	//OnEnter() defined as part of Node
+	//OnEnterSuper() defined as part of Node
+}
